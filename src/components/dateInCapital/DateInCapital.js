927df58dae67cac3aa36b8128aa9df3ec9e68da3
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-03-09 13:37:01 +0400

    feat: add template date-in-capital  component

---
+++ src/components/dateInCapital/DateInCapital.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./dateInCapital.scss";
+
+function DateInCapital() {
+  // todo get values from props or json {region: "",capital: "", country: ""}
+  const countryInfo = { region: "Europe", capital: "Paris" };
+  const { region, capital } = countryInfo;
+
+  const currentTimeZone = { timeZone: `${region}/${capital}` };
+
+  const [month, date, year] = new Date()
+    .toLocaleDateString("en-US", currentTimeZone)
+    .split("/");
+
+  const [hour, minute] = new Date()
+    .toLocaleTimeString("en-US", currentTimeZone)
+    .split(/:| /);
+
+  const addZero = (n) => {
+    return (n < 10 ? "0" : "") + n;
+  };
+
+  const capitalTitle = <p className="date-in-capital__item">{capital}</p>;
+  const capitalDate = (
+    <p className="date-in-capital__item">
+      Date: {addZero(date)}.{addZero(month)}.{year}
+    </p>
+  );
+  const capitalTime = (
+    <p className="date-in-capital__item">
+      Time: {addZero(hour)}:{addZero(minute)}
+    </p>
+  );
+
+  return (
+    <div className="date-in-capital">
+      {capitalTitle}
+      {/* add zero before number in time and in date*/}
+      {capitalDate}
+      {capitalTime}
+    </div>
+  );
+}
+
+export default DateInCapital;
---
+++ src/components/widgets/Widgets.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DateInCapital from "../dateInCapital/DateInCapital"
+
+function Widgets() {
+    return(
+        <aside className="widgets">
+            <DateInCapital />
+        </aside>
+    )
+}
+
+export default Widgets;
