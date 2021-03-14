0a3aaf05360dbc13b8801bf7f449a50c5339eedd
Yaroslav Trefilov <yaroslavtrefilov5@gmail.com>
2021-03-14 12:42:00 +0400

    fix: wait async request before render img

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -62,48 +62,84 @@ class Weather extends Component {
 
         if(this.props.language == "EN"){
             return (
-                <div className='weather'>
-                    <div className='weather__wrapper'>
-                        <img className='weather__wrapper__img' src={`http://openweathermap.org/img/w/${this.state.icon}.png`} alt='icon' />
-                        <div className='weather__wrapper__description'>{this.state.isLoaded && this.state.description}</div>
-                    </div>
-    
-                    <div className='weather__info'>
-                        <div className='weather__info__temperature'>{temp}°C</div>
-                        <div className='weather__info__wind'>wind: {this.state.wind} km/h</div>
-                        <div className='weather__info__humidity'>humidity: {this.state.humidity}%</div>
-                    </div>
+              <div className="weather">
+                <div className="weather__wrapper">
+                  {this.state.icon && (
+                    <img
+                      className="weather__wrapper__img"
+                      src={`http://openweathermap.org/img/w/${this.state.icon}.png`}
+                      alt="icon"
+                    />
+                  )}
+                  <div className="weather__wrapper__description">
+                    {this.state.isLoaded && this.state.description}
+                  </div>
                 </div>
+
+                <div className="weather__info">
+                  <div className="weather__info__temperature">{temp}°C</div>
+                  <div className="weather__info__wind">
+                    wind: {this.state.wind} km/h
+                  </div>
+                  <div className="weather__info__humidity">
+                    humidity: {this.state.humidity}%
+                  </div>
+                </div>
+              </div>
             );
         }else if(this.props.language == "RU"){
             return (
-                <div className='weather'>
-                    <div className='weather__wrapper'>
-                        <img className='weather__wrapper__img' src={`http://openweathermap.org/img/w/${this.state.icon}.png`} alt='icon' />
-                        <div className='weather__wrapper__description'>{this.state.description}</div>
-                    </div>
-    
-                    <div className='weather__info'>
-                        <div className='weather__info__temperature'>{temp}°C</div>
-                        <div className='weather__info__wind'>ветер: {this.state.wind} км/ч</div>
-                        <div className='weather__info__humidity'>влажность: {this.state.humidity}%</div>
-                    </div>
+              <div className="weather">
+                <div className="weather__wrapper">
+                  {this.state.icon && (
+                    <img
+                      className="weather__wrapper__img"
+                      src={`http://openweathermap.org/img/w/${this.state.icon}.png`}
+                      alt="icon"
+                    />
+                  )}
+                  <div className="weather__wrapper__description">
+                    {this.state.description}
+                  </div>
                 </div>
+
+                <div className="weather__info">
+                  <div className="weather__info__temperature">{temp}°C</div>
+                  <div className="weather__info__wind">
+                    ветер: {this.state.wind} км/ч
+                  </div>
+                  <div className="weather__info__humidity">
+                    влажность: {this.state.humidity}%
+                  </div>
+                </div>
+              </div>
             );
         }else if(this.props.language == "BE"){
             return (
-                <div className='weather'>
-                    <div className='weather__wrapper'>
-                        <img className='weather__wrapper__img' src={`http://openweathermap.org/img/w/${this.state.icon}.png`} alt='icon' />
-                        <div className='weather__wrapper__description'>{this.state.description}</div>
-                    </div>
-    
-                    <div className='weather__info'>
-                        <div className='weather__info__temperature'>{temp}°C</div>
-                        <div className='weather__info__wind'>вецер: {this.state.wind} км/г</div>
-                        <div className='weather__info__humidity'>вільготнасць: {this.state.humidity}%</div>
-                    </div>
+              <div className="weather">
+                <div className="weather__wrapper">
+                  {this.state.icon && (
+                    <img
+                      className="weather__wrapper__img"
+                      src={`http://openweathermap.org/img/w/${this.state.icon}.png`}
+                      alt="icon"
+                    />
+                  )}
+                  <div className="weather__wrapper__description">
+                    {this.state.description}
+                  </div>
                 </div>
+
+                <div className="weather__info">
+                  <div className="weather__info__temperature">{temp}°C</div>
+                  <div className="weather__info__wind">
+                    вецер: {this.state.wind} км/г
+                  </div>
+                  <div className="weather__info__humidity">
+                    вільготнасць: {this.state.humidity}%
+                  </div>
+                </div>
+              </div>
             );
         }
         
